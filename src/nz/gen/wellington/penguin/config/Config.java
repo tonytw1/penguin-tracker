7126ae19ffa33d904e98c566acee1990304f060f
Tony McCrae <tony@eelpieconsulting.co.uk>
2011-09-05 08:05:15 +0100

    Move location of feed to the Config class. If we're going to use a config class in this way, we might as well be consistent about it.

--- src/nz/gen/wellington/penguin/config/Config.java
+++ src/nz/gen/wellington/penguin/config/Config.java
@@ -1,7 +1,8 @@
 package nz.gen.wellington.penguin.config;
 
 public class Config {
-	
+
+	public static String FEED_URL = "http://www.nzemperor.com/rest/Position.kml";
 	public static final int CACHE_TTL = 60 * 30;
 
 }
--- src/nz/gen/wellington/penguin/data/LiveLocationService.java
+++ src/nz/gen/wellington/penguin/data/LiveLocationService.java
@@ -3,18 +3,17 @@ package nz.gen.wellington.penguin.data;
 import java.io.InputStream;
 import java.util.List;
 
+import nz.gen.wellington.penguin.config.Config;
 import nz.gen.wellington.penguin.data.kml.KMLParser;
 import nz.gen.wellington.penguin.model.Location;
 import nz.gen.wellington.penguin.network.HttpFetcher;
 import android.content.Context;
 
 public class LiveLocationService implements LocationService {
-		
-	private static String FEED_URL = "http://www.nzemperor.com/rest/Position.kml";
 	
 	public List<Location> getLocations(Context context) {
 		HttpFetcher httpFetcher = new HttpFetcher(context);
-		InputStream inputStream = httpFetcher.httpFetch(FEED_URL);
+		InputStream inputStream = httpFetcher.httpFetch(Config.FEED_URL);
 		if (inputStream != null) {
 			KMLParser parser = new KMLParser();
 			return parser.parse(inputStream);
